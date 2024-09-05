224d10387314b970e84e6eb9f5d009290409abae
renki <renkineluciano1@gmail.com>
2024-09-05 14:46:55 -0300

    Realicé el TP 4 (java).

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,11 +1,41 @@
 package com.template;
 
 public class Elevator {
+    int passengers;
+    int max_passengers;
+    int curr_floor;
+    int max_floor;
 
+    public Elevator(int passengers, int max_floor, int max_passengers) {
+        this.passengers = passengers;
+        this.max_passengers = max_passengers;
+        this.max_floor = max_floor;
+    }
 
+    public void moveToFloor(int floor) {
+        if (floor <= max_floor) {
+            this.curr_floor = floor;
+        }
+    }
 
-    // Implement this class by observing the tests in this repo. From those you will be able
-    // to pick what the constructors params are, what the methods are and what they should do.
+    public int getCurrentFloor() {
+        return curr_floor;
+    }
 
+    public void addPassenger() {
+        if (passengers < max_passengers) {
+            passengers++;
+        }
+    }
+
+    public int getPassengerCount() {
+        return passengers;
+    }
+
+    public void removePassenger() {
+        if (passengers > 0) {
+            passengers--;
+        }
+    }
 
 }
